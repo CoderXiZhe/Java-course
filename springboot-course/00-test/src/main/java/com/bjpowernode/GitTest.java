8d3265741e3b65f0df828f16450ae293bbef6863
CoderXiZhe <1974164566@qq.com>
2022-04-02 10:37:49 +0800

    git test idea

---
+++ springboot-course/00-test/src/main/java/com/bjpowernode/GitTest.java
@@ -0,0 +1,14 @@
+package com.bjpowernode;
+
+/**
+ * @author admin
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/4/2 10:36
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
